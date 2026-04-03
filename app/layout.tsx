1ff0d0fa3cd4b3c77f350e06ba157d69a16abb75
Nephesis_ <kocsorgabor@gszi.edu.hu>
2026-04-03 11:59:16 +0200

    feat. added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,13 @@ import React from 'react';
 import { Poppins } from 'next/font/google';
 import './globals.css';
 
+<link
+  rel="icon"
+  href="/logo?<generated>"
+  type="image/<generated>"
+  sizes="<generated>"
+/>
+
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '600', '700'],
